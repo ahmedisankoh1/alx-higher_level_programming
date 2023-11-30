6bae0c6894fcf031552ffe087fe6ab6fb92647f2
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-11-30 13:31:02 -0800

    task2

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    c = add(a, b)
+
+    print("{} + {} = {}".format(a, b, c))
+    c = sub(a, b)
+
+    print("{} + {} = {}".format(a, b, c))
+    c = mul(a, b)
+
+    print("{} + {} = {}".format(a, b, c))
+    c = div(a, b)
+
+    print("{} + {} = {}".format(a, b, c))
