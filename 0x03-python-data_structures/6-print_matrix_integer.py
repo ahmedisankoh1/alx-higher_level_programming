663909abb37a29e5aae726a499c63ac0bf125a4a
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-12-02 15:57:31 -0800

    hi

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for y in x:
+            print("{:d}".format(y), end=" " if y != x[-1] else "")
+        print()
