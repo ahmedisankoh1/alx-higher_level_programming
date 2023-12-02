596d8d9593abe1b4578381ae3d1eedcc591a336e
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-12-02 15:48:31 -0800

    hello

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    char_to_remove = "cC"
-    new_string = ''.join(char for char in my_string if char not in char_to_remove)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return (new_string)
 
