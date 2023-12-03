fdb5073e2415a96b6fb210e706d653cb5e7333f2
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-12-03 12:35:45 -0800

    redoing

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,8 @@
 def multiple_returns(sentence):
     str_len = len(sentence)
     if str_len < 1:
-        return (None)
+        first_char = None
     else:
         first_char = sentence[0]
-        ret_value = (str_len, first_char)
-        return (ret_value)
+    ret_value = (str_len, first_char)
+    return (ret_value)
