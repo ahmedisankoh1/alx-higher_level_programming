793041a03c389512b064b74b0855bc8645419681
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-12-03 13:09:31 -0800

    help

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    len_list = len(my_list)
+    if idx < 0 or idx >= len_list:
+        return (my_list)
+    else:
+        del my_list[idx]
+        return (my_list)
+
