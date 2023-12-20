aec4270c1996955c3948aadb26c692577df40a57
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-12-19 16:12:53 -0800

    prev

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+class Square:
+
+    def __init__(self, size=0):
+        """Initializing a new square
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            return (self.__size * self.__size)
+
