6ba9e3d27e1e1dc3f3497fb2dde56f36146b974f
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2023-12-19 16:20:57 -0800

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
         """Initialize a new Square.
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-"""Defining a class"""
+"""Define a class Square."""
+
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initializing a new square
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
@@ -16,6 +18,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            return (self.__size * self.__size)
-
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
